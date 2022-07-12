dbdf27b8568c30262c0578aef0e96a795f8fd556
Amit Verma <amit.av379@gmail.com>
2022-07-12 10:55:38 +0530

    day 12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutterproj1/models/catalog.dart';
+import 'dart:convert';
 import 'package:flutterproj1/widgets/drawer.dart';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   int days = 20;
+
+  @override
+  void initState(){
+    super.initState();
+    loadData();
+  }
+  loadData()async{
+   var catalogJson= await rootBundle.loadString("assets/files/catalog.json");
+   var decodedData= jsonDecode(catalogJson);
+   var productsData= decodedData["products"];
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
